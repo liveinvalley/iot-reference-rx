5c4f3d5386e552222fa4f277a78fa6c14b97c2f5
liveinvalley <156389201+liveinvalley@users.noreply.github.com>
2024-08-16 11:06:04 +0900

    feat: add builtin key support.

--- Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.c
+++ Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.c
@@ -6,6 +6,44 @@ psa_status_t mbedtls_psa_platform_get_builtin_key(
      psa_drv_slot_number_t *slot_number)
 {
 	switch (key_id) {
+	case PSA_KEY_ID_BUILTIN_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY:
+		*lifetime = PSA_KEY_LIFETIME_IOTREFERENCE_RX;
+		*slot_number = PSA_DRV_SLOT_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY;
+		return PSA_SUCCESS;
+
+	default:
+		return PSA_ERROR_DOES_NOT_EXIST;
+	}
+}
+
+psa_status_t psa_driver_iotreference_rx__get_key_buffer_size(
+    const psa_key_attributes_t *attributes,
+    size_t *key_buffer_size )
+{
+	*key_buffer_size = sizeof(psa_drv_slot_number_t);
+	return PSA_SUCCESS;
+}
+
+psa_status_t psa_driver_iotreference_rx_get_builtin_key(
+    psa_drv_slot_number_t slot_number,
+    psa_key_attributes_t *attributes,
+    uint8_t *key_buffer, size_t key_buffer_size, size_t *key_buffer_length )
+{
+	switch (slot_number) {
+	case PSA_DRV_SLOT_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY:
+		// TODO: partially hardcoded...
+		psa_set_key_type(attributes, PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1));
+		psa_set_key_bits(attributes, 256);
+		psa_set_key_usage_flags(attributes,PSA_KEY_USAGE_SIGN_HASH);
+		psa_set_key_algorithm(attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+
+		if (key_buffer_size < sizeof(psa_drv_slot_number_t)) {
+			return PSA_ERROR_BUFFER_TOO_SMALL;
+		}
+		*((psa_drv_slot_number_t *) key_buffer) = PSA_DRV_SLOT_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY;
+		*key_buffer_length = sizeof(psa_drv_slot_number_t);
+		return PSA_SUCCESS;
+
 	default:
 		return PSA_ERROR_DOES_NOT_EXIST;
 	}
--- Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.h
+++ Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.h
@@ -3,9 +3,25 @@
 
 #include <psa/crypto.h>
 
+#define PSA_KEY_ID_BUILTIN_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY MBEDTLS_PSA_KEY_ID_BUILTIN_MIN
+
+#define PSA_KEY_LOCATION_IOTREFERENCE_RX 0xf0
+#define PSA_KEY_LIFETIME_IOTREFERENCE_RX PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(PSA_KEY_PERSISTENCE_READ_ONLY, PSA_KEY_LOCATION_IOTREFERENCE_RX)
+
+#define PSA_DRV_SLOT_IOTREFERENCE_RX_DEVICE_PRIVATE_KEY 0
+
 psa_status_t mbedtls_psa_platform_get_builtin_key(
      mbedtls_svc_key_id_t key_id,
      psa_key_lifetime_t *lifetime,
      psa_drv_slot_number_t *slot_number);
+
+psa_status_t psa_driver_iotreference_rx__get_key_buffer_size(
+    const psa_key_attributes_t *attributes,
+    size_t *key_buffer_size );
+
+psa_status_t psa_driver_iotreference_rx_get_builtin_key(
+    psa_drv_slot_number_t slot_number,
+    psa_key_attributes_t *attributes,
+    uint8_t *key_buffer, size_t key_buffer_size, size_t *key_buffer_length );
 
 #endif /* PSA_BUILTIN_KEYS_H_ */
