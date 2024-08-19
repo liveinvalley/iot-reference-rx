10620b51f0d769f11b8bad0344231a770037e33e
liveinvalley <156389201+liveinvalley@users.noreply.github.com>
2024-08-19 09:31:22 +0900

    feat: add export public key support to PSA driver.

--- Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.h
+++ Projects/aws_da16600_ck_rx65n/e2studio_gcc/src/application_code/psa_builtin_keys.h
@@ -34,4 +34,9 @@ psa_status_t psa_driver_iotreference_rx_generate_key(
     const psa_key_attributes_t *attributes,
     uint8_t *key_buffer, size_t key_buffer_size, size_t *key_buffer_length );
 
+psa_status_t psa_driver_iotreference_rx_export_public_key(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key_buffer, size_t key_buffer_size,
+    uint8_t *data, size_t data_size, size_t *data_length );
+
 #endif /* PSA_BUILTIN_KEYS_H_ */
